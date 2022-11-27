13e432667de813671cc3116b869c4b48275c2a75
nadas123x <77413627+nadas123x@users.noreply.github.com>
2022-11-27 23:28:22 +0100

    premier test

---
+++ src/Count.js
@@ -0,0 +1,46 @@
+
+import React, { useState } from "react";
+import "./Count.css";
+
+const Count = () => {
+
+const [counter, setCounter] = useState(0)
+
+const [text, setText] = useState('black');
+
+const increment = () => {
+	setCounter(counter + 1)
+  setText(counter >= 0 ? 'blue':'red' );
+
+}
+
+const decrement = () => {
+	setCounter(counter - 1)
+  setText(counter <= 0 ? 'red' : 'blue');
+
+}
+
+return (
+	<div className="page" >
+	<div >
+		  <small style={{ color:text}} className="container_text">{counter}</small>
+	</div>
+  <div >
+
+    
+		<button className="btn2"
+		onClick={decrement}><span>-</span></button>
+
+    &nbsp;&nbsp;
+
+
+		<button className="btn"
+		onClick={increment}>+</button>
+	</div>
+
+	</div>
+
+)
+}
+
+export default Count;
